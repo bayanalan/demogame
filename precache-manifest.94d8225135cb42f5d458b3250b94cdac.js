27a79752933a6ce85b17b9678eb802c055998c21
bayanalan <bayan.alan.jones@gmail.com>
2021-04-18 12:24:31 -0700

    gitploy

---
+++ precache-manifest.94d8225135cb42f5d458b3250b94cdac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53e6d0ca429cd74b3c6ade1305e3688",
+    "url": "/demogame/index.html"
+  },
+  {
+    "revision": "40250b288278dcf509e0",
+    "url": "/demogame/static/css/main.a421fb84.chunk.css"
+  },
+  {
+    "revision": "40250b288278dcf509e0",
+    "url": "/demogame/static/js/main.bfb66cc8.chunk.js"
+  },
+  {
+    "revision": "1e04a17d4aec47809c7e",
+    "url": "/demogame/static/js/runtime-main.8ed61385.js"
+  },
+  {
+    "revision": "05fd3ce5329a2268229eaf44910bf72d",
+    "url": "/demogame/static/media/dude.526a78a9.png"
+  },
+  {
+    "revision": "fc7d654a5cbca97fbe6091f9b37ac135",
+    "url": "/demogame/static/media/platform.69db09bf.png"
+  },
+  {
+    "revision": "cb8fe21064e648d4fd4aa71e2a5d339d",
+    "url": "/demogame/static/media/sky.47a86b19.png"
+  },
+  {
+    "revision": "3bbfad7412f062fcbae5984254093294",
+    "url": "/demogame/static/media/star.a7cb585c.png"
+  }
+]);
